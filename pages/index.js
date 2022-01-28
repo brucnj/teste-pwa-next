e78b3cf68cb20d6148cf66a2a08e4380623d2432
Bruna Conejo <72928042+brucnj@users.noreply.github.com>
2022-01-28 16:25:43 -0300

    Teste Camera 2

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,33 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import { useState, useEffect } from 'react'
 
 export default function Home() {
+  const [activeMedia, setMedia] = useState(false);
+  
+  useEffect(() => {
+    async function media(){
+      const videoMedia = await navigator.mediaDevices.getUserMedia({
+        video: true
+      });
+  
+      document.querySelector('video').srcObject = new MediaStream(videoMedia);
+    }
+
+    media();
+  });
+
+
   return (
     <div className={styles.container}>
       <div className={styles.container}>
         <Link href="/lp/kwid">
-          <a className="bg-stone-900 py-5 px-10 mx-auto text-white text-lg font-bold leading-7">Ir para Landing Kwid</a>
+          <a className="px-10 py-5 mx-auto text-lg font-bold leading-7 text-white bg-stone-900">Ir para Landing Kwid</a>
         </Link>
+
+        <button onClick={() => setMedia(true)} className="px-10 py-5 mx-auto mt-10 text-lg font-bold leading-7 text-white bg-stone-900"> Abrir Camera</button>
+        <video autoPlay></video>
       </div>
     </div>
   )
