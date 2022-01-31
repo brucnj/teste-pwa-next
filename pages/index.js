73ac41504bacb385576819d735961f318da64f1e
Bruna Conejo <72928042+brucnj@users.noreply.github.com>
2022-01-31 10:50:55 -0300

    Teste Camera 6

--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,46 @@ import Link from 'next/link'
 import { useState, useEffect } from 'react'
 
 export default function Home() {
-  const [source, setSource] = useState("");
-  const handleCapture = (target) => {
-    if(target.files) {
-      if(target.files.length !== 0 ){
-        const file = target.files[0];
-        const newUrl = URL.createObjectURL(file);
-        setSource(newUrl);
-      }
-    }
-  }
+  //Teste  6
+  useEffect(() => {
+    var canvas = document.getElementById("canvas"),
+    context = canvas.getContext("2d"),
+    video = document.getElementById("video"),
+    videoObj = { "video": true },
+    errBack = function (error) {
+      console.log("Video capture error: ", error.code);
+    };
 
+    // Put video listeners into place
+    if (navigator.getUserMedia) { // Standard
+        navigator.getUserMedia(videoObj, function (stream) {
+            video.src = stream;
+            video.play();
+        }, errBack);
+    } 
+
+    document.getElementById("snap")
+            .addEventListener("click", function() {
+    setInterval(function(){
+      context.drawImage(video, 0, 0, 640, 480);
+    },1)
+  });
+  }, []);
+
+  //Teste 5
+  // const [source, setSource] = useState("");
+  // const handleCapture = (target) => {
+  //   if(target.files) {
+  //     if(target.files.length !== 0 ){
+  //       const file = target.files[0];
+  //       const newUrl = URL.createObjectURL(file);
+  //       setSource(newUrl);
+  //     }
+  //   }
+  // }
+
+  //Teste 4
   // const [activeMedia, setMedia] = useState(false);
-  
   // useEffect(() => {
   //   async function media(){
   //     if(activeMedia){
@@ -34,10 +61,19 @@ export default function Home() {
   //   media();
   // });
 
-
+  
   return (
     <>
-      <div className={styles.container}>
+      {/* Teste 6 */}
+      <div className={ styles.container }>
+        <video id="video" width="640" height="480" autoplay></video>
+        <button id="snap">Snap Photo</button>
+        <canvas id="canvas" width="640" height="480"></canvas>
+      </div>
+
+
+      {/* Teste 5 - Abre a câmera do dispositivo (mobile apenas) e anexa a foto */}
+      {/* <div className={styles.container}>
         <h5>Teste de Câmera</h5>
         {source &&
           <div className="flex justify-center border-black border-1">
@@ -49,9 +85,9 @@ export default function Home() {
           type="file"
           capture="environment"
           onChange={(e) => handleCapture(e.target)} />
+      </div> */}
 
-      </div>
-
+      {/* Teste 4 - abre a câmera e exibe o vídeo (não funciona PWA, somente Desktop) */}
       {/* <Script src="https://webrtc.github.io/adapter/adapter-latest.js"></Script>
       <div className={styles.container}>
         <div className={styles.container}>
@@ -66,6 +102,5 @@ export default function Home() {
         </div>
       </div> */}
     </>
-   
   )
 }
