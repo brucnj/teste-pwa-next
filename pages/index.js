0a673f14d58d3ddc7c800aca52ceaa80fb4235f9
Bruna Conejo <72928042+brucnj@users.noreply.github.com>
2022-01-31 08:26:48 -0300

    Teste Camera 5

--- pages/index.js
+++ pages/index.js
@@ -4,38 +4,67 @@ import Link from 'next/link'
 import { useState, useEffect } from 'react'
 
 export default function Home() {
-  const [activeMedia, setMedia] = useState(false);
-  
-  useEffect(() => {
-    async function media(){
-      if(activeMedia){
-        const videoMedia = await navigator.mediaDevices.getUserMedia({
-          video: true
-        });
-        const videoTracks = videoMedia.getVideoTracks()
-        const track = videoTracks[0]
-        alert(`Getting video from: ${track.label}`)
-        document.querySelector('video').srcObject = new MediaStream(videoMedia);
+  const [source, setSource] = useState("");
+  const handleCapture = (target) => {
+    if(target.files) {
+      if(target.files.length !== 0 ){
+        const file = target.files[0];
+        const newUrl = URL.createObjectURL(file);
+        setSource(newUrl);
       }
     }
+  }
 
-    media();
-  });
+  // const [activeMedia, setMedia] = useState(false);
+  
+  // useEffect(() => {
+  //   async function media(){
+  //     if(activeMedia){
+  //       const videoMedia = await navigator.mediaDevices.getUserMedia({
+  //         audio: false,
+  //         video: true
+  //       });
+  //       const videoTracks = videoMedia.getVideoTracks()
+  //       const track = videoTracks[0]
+  //       alert(`Getting video from: ${track.label}`)
+  //       document.querySelector('video').srcObject = new MediaStream(videoMedia);
+  //     }
+  //   }
+
+  //   media();
+  // });
 
 
   return (
     <>
-      <Script src="https://webrtc.github.io/adapter/adapter-latest.js"></Script>
+      <div className={styles.container}>
+        <h5>Teste de Câmera</h5>
+        {source &&
+          <div className="flex justify-center border-black border-1">
+            <img src={source} alt={"snap"}></img>
+          </div>
+        } 
+        <input 
+          accept='image/*'
+          type="file"
+          capture="environment"
+          onChange={(e) => handleCapture(e.target)} />
+
+      </div>
+
+      {/* <Script src="https://webrtc.github.io/adapter/adapter-latest.js"></Script>
       <div className={styles.container}>
         <div className={styles.container}>
           <Link href="/lp/kwid">
             <a className="px-10 py-5 mx-auto text-lg font-bold leading-7 text-white bg-stone-900">Ir para Landing Kwid</a>
           </Link>
 
-          <button onClick={() => setMedia(true)} className="px-10 py-5 mx-auto mt-10 text-lg font-bold leading-7 text-white bg-stone-900"> Abrir Camera</button>
+          <button onClick={() => setMedia(true)} className="px-10 py-5 mx-auto mt-10 text-lg font-bold leading-7 text-white bg-stone-900">Abrir Camera</button>
           <video autoPlay></video>
+
+          <input type="file" accept="image/*" capture="camera" />
         </div>
-      </div>
+      </div> */}
     </>
    
   )
