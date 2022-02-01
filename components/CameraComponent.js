d5c430bab34e2c5ff0457dd1ebec6106cc5e6cc1
Bruna Conejo <72928042+brucnj@users.noreply.github.com>
2022-02-01 16:55:55 -0300

    Alterações (Camera Implementação antiga)

--- components/CameraComponent.js
+++ components/CameraComponent.js
@@ -4,37 +4,37 @@ import Image from 'next/image'
 import { useEffect } from 'react/cjs/react.development'
 
 const CameraComponent = () => {
-    // const [imageReady, setImageReady] = useState({ isReady: false, url: "" })
-    // async function takePicture() {
-    //     const image = await Camera.getPhoto({
-    //         quality: 90,
-    //         allowEditing: true,
-    //         resultType: CameraResultType.Uri
-    //     });
-    //     var imageUrl = image.webPath;
-    //     // imageElement.src = imageUrl;
-    //     setImageReady({ isReady: true, url: imageUrl });
-    // };
-
-    function getVideo(){
-        const player = document.getElementById('player');
-
-        navigator.mediaDevices.getUserMedia({ video: true })
-        .then((stream) => {
-          player.srcObject = stream;
+    const [imageReady, setImageReady] = useState({ isReady: false, url: "" })
+    async function takePicture() {
+        const image = await Camera.getPhoto({
+            quality: 90,
+            allowEditing: true,
+            resultType: CameraResultType.Uri
         });
-    }
+        var imageUrl = image.webPath;
+        // imageElement.src = imageUrl;
+        setImageReady({ isReady: true, url: imageUrl });
+    };
+
+    // function getVideo(){
+    //     const player = document.getElementById('player');
+
+    //     navigator.mediaDevices.getUserMedia({ video: true })
+    //     .then((stream) => {
+    //       player.srcObject = stream;
+    //     });
+    // }
 
     return(
         <>
-            <button onClick={getVideo}>Get Video</button>
-            <video id="player" controls autoPlay></video>
-            {/* <button onClick={takePicture} className="px-10 py-5 text-white bg-stone-900"> Tirar Foto</button>
+            {/* <button onClick={getVideo}>Get Video</button>
+            <video id="player" controls autoPlay></video> */}
+            <button onClick={takePicture} className="px-10 py-5 text-white bg-stone-900"> Tirar Foto</button>
             {imageReady.isReady && 
                 <>
                     <img alt="foto" src={imageReady.url} />
                 </>
-            } */}
+            }
         </>
     )
 }
