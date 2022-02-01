63f7e44c466130923240eacb665e1fd98fa95693
Bruna Conejo <72928042+brucnj@users.noreply.github.com>
2022-02-01 16:34:11 -0300

    Teste Camera

--- components/CameraComponent.js
+++ components/CameraComponent.js
@@ -1,27 +1,37 @@
 import { Camera, CameraResultType } from '@capacitor/camera'
 import { useState } from 'react'
 import Image from 'next/image'
+import { useEffect } from 'react/cjs/react.development'
 
 const CameraComponent = () => {
-    const [imageReady, setImageReady] = useState({ isReady: false, url: "" })
-    async function takePicture() {
-        const image = await Camera.getPhoto({
-            quality: 90,
-            allowEditing: true,
-            resultType: CameraResultType.Uri
+    // const [imageReady, setImageReady] = useState({ isReady: false, url: "" })
+    // async function takePicture() {
+    //     const image = await Camera.getPhoto({
+    //         quality: 90,
+    //         allowEditing: true,
+    //         resultType: CameraResultType.Uri
+    //     });
+    //     var imageUrl = image.webPath;
+    //     // imageElement.src = imageUrl;
+    //     setImageReady({ isReady: true, url: imageUrl });
+    // };
+        const player = document.getElementById('player');
+
+        navigator.mediaDevices.getUserMedia({ video: true })
+        .then((stream) => {
+          player.srcObject = stream;
         });
-        var imageUrl = image.webPath;
-        // imageElement.src = imageUrl;
-        setImageReady({ isReady: true, url: imageUrl });
-    };
+
     return(
         <>
-            <button onClick={takePicture} className="px-10 py-5 text-white bg-stone-900"> Tirar Foto</button>
+            {/* <button onClick={getVideo}>Get Video</button> */}
+            <video id="player" controls autoPlay></video>
+            {/* <button onClick={takePicture} className="px-10 py-5 text-white bg-stone-900"> Tirar Foto</button>
             {imageReady.isReady && 
                 <>
                     <img alt="foto" src={imageReady.url} />
                 </>
-            }
+            } */}
         </>
     )
 }
--- pages/features.js
+++ pages/features.js
@@ -21,7 +21,7 @@ export default function Features() {
                 </div>
                 <div>
                     <h2 className="mb-5 text-3xl font-bold">Teste Vibração</h2>
-                    <button onClick={() => {navigator.vibrate(2000)}} className="px-10 py-5 text-white bg-stone-900">Teste de Vibração</button>
+                    <button onClick={() => {navigator.vibrate(5000)}} className="px-10 py-5 text-white bg-stone-900">Teste de Vibração</button>
                 </div>
             </div>
         </div>
